457b7dc81de254f442bd2a9152a94df305751c4b
carav008 <carav008@morris.umn.edu>
2017-12-03 14:07:24 -0600

    Add hello message to server

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,25 @@
 package segmentedfilesystem;
 
-public class Main {
-    
-    public static void main(String[] args) {
-        
-    }
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int port = 6014;
+		InetAddress address = InetAddress.getByName("146.57.33.55");
+		
+		DatagramSocket socket = new DatagramSocket();
+		
+		//Say hello to server
+		byte[] buffer = new byte[1024];
+		DatagramPacket packetToSend = new DatagramPacket(buffer, buffer.length, address, port);   
+		socket.send(packetToSend);
+		
+	}
 }
+
